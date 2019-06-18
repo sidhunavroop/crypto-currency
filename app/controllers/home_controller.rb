763fe9d0f7637c66d3e85e980b0bacadcb9dda1f
Navroop Singh Sidhu <sidhunavroop@yahoo.com>
2019-06-17 21:07:21 -0400

    Connection to api generated

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,36 @@
 class HomeController < ApplicationController
   def index
+    require 'net/http'
+    require 'json'
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @coins = JSON.parse(@response)
   end
 
   def about
+  end
+
+  def lookup
+    require 'net/http'
+
+    require 'json'
+
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+
+    @uri = URI(@url)
+
+    @response = Net::HTTP.get(@uri)
+
+    @lookup_coin = JSON.parse(@response)
+
+    @symbol = params[:sym]
+    if @symbol
+      @symbol = @symbol.upcase
+    end
+
+    if @symbol == ""
+      @symbol = "Hey you forgot to enter a currency"
+    end
   end
 end
